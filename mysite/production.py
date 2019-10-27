a298208d91408ff80662737ea4ab366719b53ab9
Kyle Lehning <lehnink@gmail.com>
2019-10-27 12:11:08 -0700

    Updated DATABASE_URL

--- mysite/production.py
+++ mysite/production.py
@@ -13,4 +13,4 @@ ALLOWED_HOSTS = ['python230-ubuntu8000005.westus.cloudapp.azure.com', 'localhost
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = 'adkDf9brpZSvyY6F5sxqE7F579pPGL72y98AaDCaBQ9dHZ9XBT'
 # SECRET_KEY = os.environ.get('SECRET_KEY', 0)
-DATABASE_URL = 'postgres://postgres:8dB8t!c7bFHFnThYw@<db DNS>:5432/djangoblog'
+DATABASE_URL = 'postgres://postgres:8dB8t!c7bFHFnThYw@python-2303818414376005.westus.cloudapp.azure.com:5432/djangoblog'

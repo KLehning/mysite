62871c25f1ab75ad589078edfe735124fb9e40c7
Kyle Lehning <lehnink@gmail.com>
2019-10-26 21:34:53 -0700

    updated DATABASES

--- mysite/settings.py
+++ mysite/settings.py
@@ -92,12 +92,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 DATABASE_URL='postgres://postgres:8dB8t!c7bFHFnThYw@python-2303818414376004.westus.cloudapp.azure.com/djangoblog'
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'default': dj_database_url.config(default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')),
+    'default': dj_database_url.config(default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'))
     }
-}
+
+
+#DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   # }
+#}
 
 
 # Password validation

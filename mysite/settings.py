7f7471034c0a1004ffedb96424e721d69e7e16d7
Kyle Lehning <lehnink@gmail.com>
2019-10-26 21:01:45 -0700

    uncommented SECRET_KEY in settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '1g#1!70p==#%&b77ppckaw$grlz%v(ye3%*h%6r5m)3r%n-j@k'
+SECRET_KEY = '1g#1!70p==#%&b77ppckaw$grlz%v(ye3%*h%6r5m)3r%n-j@k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
